e9e88160d5cc8c483087de40d59b8067f6a9431a
salman-41 <salman.yz@outlook.com>
2023-11-18 21:11:55 +0500

    feat: event listeneres

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ const dateEl = document.getElementById('date-picker');
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+//event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
