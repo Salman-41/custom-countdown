595bc5d3cf2f164f8bbd58e02135fa7231251577
salman-41 <salman.yz@outlook.com>
2023-11-18 21:19:38 +0500

    feat: set date input min with todays date

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const dateEl = document.getElementById('date-picker');
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Min with Today's Date
 function updateCountdown(e) {
   e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[0].value;
 }
+
 //Event Listeners
 countdownForm.addEventListener('submit', updateCountdown);
