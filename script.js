dfa11652111ac3dc1b8697dc1d9f9d837f87e2ed
salman-41 <salman.yz@outlook.com>
2023-11-20 21:05:11 +0500

    feat: populating countdown

--- script.js
+++ script.js
@@ -27,6 +27,18 @@ function updateDOM() {
   const hours = Math.floor((distance % day) / hour);
   const minutes = Math.floor((distance % hour) / minute);
   const seconds = Math.floor((distance % minute) / second);
+
+  // Populate Countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+
+  //Hide Input
+  inputContainer.hidden = true;
+  //Show Countdown
+  countdownEl.hidden = false;
 }
 
 // Set Date Input Min with Today's Date
