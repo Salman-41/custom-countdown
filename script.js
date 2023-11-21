5c3389de1f44de421f73c87c056761f4eef14aeb
salman-41 <salman.yz@outlook.com>
2023-11-21 21:24:38 +0500

    feat: adding setInterval for every second

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ dateEl.setAttribute('min', today);
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
+let countdownActive;
 
 const second = 1000;
 const minute = second * 60;
@@ -21,31 +22,33 @@ const day = hour * 24;
 
 // Populate Countdown / Complete UI
 function updateDOM() {
-  const now = new Date().getTime();
-  const distance = countdownValue - now;
-  const days = Math.floor(distance / day);
-  const hours = Math.floor((distance % day) / hour);
-  const minutes = Math.floor((distance % hour) / minute);
-  const seconds = Math.floor((distance % minute) / second);
+  countdownActive = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+    const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
 
-  // Populate Countdown
-  countdownElTitle.textContent = `${countdownTitle}`;
-  timeElements[0].textContent = `${days}`;
-  timeElements[1].textContent = `${hours}`;
-  timeElements[2].textContent = `${minutes}`;
-  timeElements[3].textContent = `${seconds}`;
+    // Populate Countdown
+    countdownElTitle.textContent = `${countdownTitle}`;
+    timeElements[0].textContent = `${days}`;
+    timeElements[1].textContent = `${hours}`;
+    timeElements[2].textContent = `${minutes}`;
+    timeElements[3].textContent = `${seconds}`;
 
-  //Hide Input
-  inputContainer.hidden = true;
-  //Show Countdown
-  countdownEl.hidden = false;
+    //Hide Input
+    inputContainer.hidden = true;
+    //Show Countdown
+    countdownEl.hidden = false;
+  });
 }
 
 // Set Date Input Min with Today's Date
 function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.srcElement[0].value;
-  countdownDate = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
   // Get number version of current Date, updateDom
   countdownValue = new Date(countdownDate).getTime();
   updateDOM();
