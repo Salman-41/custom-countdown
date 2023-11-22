1b8d26a982373a62c15eaad5d2d9ee515856e29d
salman-41 <salman.yz@outlook.com>
2023-11-22 21:12:02 +0500

    feat: adding DOM element

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const countdownEl = document.getElementById('countdown');
 const countdownElTitle = document.getElementById('countdown-title');
 const countdownBtn = document.getElementById('countdown-button');
 const timeElements = document.querySelectorAll('span');
+const completeEl = document.getElementById('complete');
+const completeElInfo = document.getElementById('complete-info');
+const completeBtn = document.getElementById('complete-button');
 
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
